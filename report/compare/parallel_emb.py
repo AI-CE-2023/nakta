8838fd74cbb00afc72056f8ef026c269962317ec
delibae <iwdhanjin@gmail.com>
2023-10-02 05:21:08 +0900

    for report

--- report/compare/parallel_emb.py
+++ report/compare/parallel_emb.py
@@ -4,8 +4,8 @@ import seaborn as sns
 
 # 데이터 생성
 data = {
-    "Name": ["SwiGLU", "Fused SwiGLU"],
-    "Duration": ["32.655 ms", "26.445 ms"],
+    "Name": ["LLAMA", "Nakta", "Nakta with Cache"],
+    "Duration": ["1784 ms", "956 ms", "751 ms"],
 }
 
 df = pd.DataFrame(data)
@@ -19,16 +19,17 @@ df["Duration"] = df["Duration"].apply(
 colors = sns.color_palette("Set2", len(df["Name"]))
 # 속도 (연산속도 = 1/시간) 계산
 df["Speed"] = 1 / df["Duration"]
-title = "SwiGLU"
+title = "Test 2"
 # 바 그래프 그리기
 plt.bar(df["Name"], df["Speed"], color=colors)
-plt.ylabel("Speed (1/ms)")
+plt.ylabel("Speed (1/s)")
 plt.title(title)
-# plt.savefig(f"./graph/{title}.png")
+plt.tight_layout()
+plt.savefig(f"./graph/{title}.png")
 plt.show()
 
 # 속도 향상률 계산
-speed_increase = ((df.loc[1, "Speed"] - df.loc[0, "Speed"]) / df.loc[0, "Speed"]) * 100
+speed_increase = ((df.loc[2, "Speed"] - df.loc[0, "Speed"]) / df.loc[0, "Speed"]) * 100
 
 # 결과 출력
 print(f"Fused SwiGlu is {speed_increase:.2f}% faster than SwiGLU.")
